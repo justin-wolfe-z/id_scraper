ae42489885bc02001b7e1217882dc15b2ca79d43
justin-wolfe-z <justin.wolfe@zapier.com>
2019-05-01 14:30:14 -0400

    better email name error handling

--- unminified.js
+++ unminified.js
@@ -6,21 +6,30 @@ var outputNode = smallerChunk[0];
 $(".choices").children().each(function(){
     var value = $(this).find(".choice-sample").text();
     if(value){
-      var key = $(this).find(".choice-label").children().first().children().first().text();
+      const label = $(this).find(".choice-label")
+      let key = ''
+      if(label.length > 1){
+        key = label.children().first().children().first().text();
+      } else {
+        key = label.text()
+      }
       var outputRow = '"'+key+'",'+'"'+value+'"';
       outputCSV += outputRow + "\r\n";
       outputHTML += outputRow + "<br>";
     }
 });
-var email = prompt("What's your Zapier email?");
-if(email){
-  $.get( "https://hooks.zapier.com/hooks/catch/2003878/jhmw54/", { email: email, node: outputNode, csv: outputCSV }) 
+var username = prompt("What's your Zapier email?");
+if(username){
+  while(!username.includes('@') || username[0] === "@"){
+    username = prompt("Are you sure that's your Zapier email? It looks like it might be a Slack username. Please try reentering it");
+  }
+  $.get( "https://hooks.zapier.com/hooks/catch/2003878/zyunoo/", { slack: username, node: outputNode, csv: outputCSV }) 
   .done(function(){
     alert("Successfully sent to Zapbot! You'll get a DM with a link to your CSV in just a sec");
   })
   .fail(function(){
     alert("Bargle! GET request to Zapier failed, probably because the CSV is too long :(. No worries, I'm going to open two new tabs—one with the CSV for you to copy and one for a form where you'll paste that CSV in!");
-    window.open("https://zapier.formstack.com/forms/csv_generator_backup_copy?email="+email+"&node="+outputNode);
+    window.open("https://zapier.formstack.com/forms/csv_generator_backup?slack="+username+"&node="+outputNode);
     window.open().document.write(outputHTML);
   })
 }
